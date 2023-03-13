b08201064492b28cb9bbe93c7cc9531f641c70bf
Reena Kahar <reenabadera8999@gmail.com>
2023-03-13 18:36:56 +0530

    Initial commit

---
+++ src/main/java/com/example/Test12MarcApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test12MarcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test12MarcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/dao/UserRepo.java
@@ -0,0 +1,11 @@
+package com.example.dao;
+
+
+
+import com.example.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users,Integer> {
+}
